e3e240a9abecace0a73d83373b8425860ea4f829
hoho0161 <hoho0161@naver.com>
2023-03-21 18:03:36 +0900

    update 배포자동화코드

--- src/main/java/com/jojoidu/book/springboot/Application.java
+++ src/main/java/com/jojoidu/book/springboot/Application.java
@@ -2,12 +2,14 @@ package com.jojoidu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 
 @SpringBootApplication
 public class Application {
     public static void main(String [] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
